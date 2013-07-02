13bc38d8474f0d9ae1d05e765dc4dcc0ce2e9775
dcramer <david.cramer@rackspace.com>
2013-07-02 16:20:40 -0500

    Update to use xinclude and sort them correctly.

--- convert.sh
+++ convert.sh
@@ -13,41 +13,14 @@ mkdir -p $DEST_DIR
 cp -r $SOURCE_DIR/static $DEST_DIR/
 cp pom.xml $DEST_DIR/
 
-# TODO: Convert CALS tables to html?
-
 cat > $BOOK_FILE <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE book [
-EOF
-
-# Adding "| sort" so chapters will be in correct order
-for file in `find $SOURCE_DIR -maxdepth 1 -iname '*.html' -print | sort`;
-do
-    BASE=`basename $file .html`
-    OUTPUT=$DEST_DIR/$BASE.xml
-    # I installed herold using apt-get on Ubuntu, so using the wrapper script
-    # Also, I'm genrating $OUTPUT.tmp so I can further process the output file.
-    # Remove <br>s from source files. These appear in <pre> element for some reason.
-    # There's one src attribute on an image that has a full, 
-    # but malformed url instead of a relative path. Cleaning that up.
-    sed 's/<br>//' $file | sed 's#http:/openstack.booktype.pro/security-guide/_full/###' > $file.tmp
-    # java -jar $HEROLD -p $PROFILE -i $file.tmp -o $OUTPUT.tmp
-    herold -i $file.tmp -o $OUTPUT.tmp
-    rm $file.tmp
-    # Now process with an xslt to:
-    # 1. Remove <article> wrappers
-    # 2. Convert top level <section> to <chapter>
-    # 3. Add xml:id attributes to chapter and sections
-    xsltproc --stringparam BASE $BASE cleanup.xsl $OUTPUT.tmp > $OUTPUT
-    # Cleanup temp file:
-    rm $OUTPUT.tmp 
-    echo "<!ENTITY ${BASE} SYSTEM \"${BASE}.xml\">" >> $BOOK_FILE
-done
-
-cat >> $BOOK_FILE <<EOF
-]>
-<book version="5.0" xml:id="os-security-guide" xmlns="http://docbook.org/ns/docbook" xmlns:xlink="http://www.w3.org/1999/xlink">
-<title>OpenStack Security Guide</title>
+<book version="5.0" 
+      xml:id="os-security-guide" 
+      xmlns:xi="http://www.w3.org/2001/XInclude"
+      xmlns="http://docbook.org/ns/docbook" 
+      xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title>OpenStack Security Guide</title>
     <info>
         <author>
             <personname>
@@ -88,13 +61,30 @@ cat >> $BOOK_FILE <<EOF
             </revision>
         </revhistory>
     </info>
-
 EOF
 
-for file in `find $SOURCE_DIR -iname '*.html' -print`;
+# Adding "| sort" so chapters will be in correct order
+for file in `find $SOURCE_DIR -maxdepth 1 -iname '*.html' -print | sort`;
 do
     BASE=`basename $file .html`
-    echo "&${BASE};" >> $BOOK_FILE
+    OUTPUT=$DEST_DIR/$BASE.xml
+    # I installed herold using apt-get on Ubuntu, so using the wrapper script
+    # Also, I'm genrating $OUTPUT.tmp so I can further process the output file.
+    # Remove <br>s from source files. These appear in <pre> element for some reason.
+    # There's one src attribute on an image that has a full, 
+    # but malformed url instead of a relative path. Cleaning that up.
+    sed 's/<br>//' $file | sed 's#http:/openstack.booktype.pro/security-guide/_full/###' > $file.tmp
+    # java -jar $HEROLD -p $PROFILE -i $file.tmp -o $OUTPUT.tmp
+    herold -i $file.tmp -o $OUTPUT.tmp
+    rm $file.tmp
+    # Now process with an xslt to:
+    # 1. Remove <article> wrappers
+    # 2. Convert top level <section> to <chapter>
+    # 3. Add xml:id attributes to chapter and sections
+    xsltproc --stringparam BASE $BASE cleanup.xsl $OUTPUT.tmp > $OUTPUT
+    # Cleanup temp file:
+    rm $OUTPUT.tmp 
+    echo "<xi:include href='${BASE}.xml'/>" >> $BOOK_FILE
 done
 
 cat >> $BOOK_FILE <<EOF
